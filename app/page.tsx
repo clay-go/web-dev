ea0cfe3930a72089cfee25e8fc72b379c52abc48
Clay <clay.phan.goldenowl@gmail.com>
2023-11-16 01:18:33 +0700

    add post

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,110 @@
-import Image from 'next/image'
+"use client";
+import { useState, useEffect, useContext } from "react";
+// import PostsContext from "../context/PostsContext";
+import axios from "axios";
+import Tag from "@/components/cards/Tag";
+import BlogCard from "@/components/cards/BlogCard";
+import PostsContext from "@/context/PostsContext";
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+  const [posts, setPosts] = useState([]);
+  const [selectedTags, setSelectedTags] = useState([]);
+  const [error, setError] = useState();
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+  const { setPosts: postContextFunc } = useContext(PostsContext);
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const data = await axios.get("/api/posts");
+        setPosts(data.data);
+        postContextFunc(data.data);
+      } catch (error) {
+        setError("Error fetching posts");
+      }
+    }
+
+    fetchData();
+  }, []);
+
+  if (posts?.length === 0) {
+    return <div className="text-center">No posts found...</div>;
+  }
+
+  if (error) {
+    return (
+      <div className="text-center text-3xl mt-10">
+        <img
+          src="https://images.unsplash.com/photo-1555861496-0666c8981751?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2970&q=80"
+          alt="error"
+          className="w-96 h-96 object-cover"
         />
       </div>
+    );
+  }
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+  return (
+    <main className="min-h-screen p-10 lg:p:20">
+      <h2 className="text-center text-3xl md:text-4xl lg:text-5xl font-bold mb-10 flex flex-col justify-center items-center uppercase tracking-widest h-96">
+        <span className="text-5xl border-b-4 pb-3 font-bold">Dev Blog</span>
+      </h2>
+      <h2 className="flex flex-wrap mt-10 gap-4">
+        {[
+          ...new Set(
+            posts?.map((post) => {
+              return post.tag;
+            })
+          ),
+        ].map((tag) => {
+          return (
+            <Tag
+              key={tag}
+              tag={tag}
+              isSelected={selectedTags.includes(tag)}
+              setSelectedTags={setSelectedTags}
+            />
+          );
+        })}
+        {selectedTags?.length !== 0 && (
+          <button
+            className="bg-red-500 text-white px-4 py-2 rounded-md"
+            onClick={() => setSelectedTags([])}
+          >
+            Clear
+          </button>
+        )}
+      </h2>
+      <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-4 mt-10">
+        {selectedTags?.length !== 0 &&
+          posts
+            ?.filter((post) => {
+              return selectedTags.includes(post.tag);
+            })
+            .map((post) => {
+              return (
+                <BlogCard
+                  tag={post.tag}
+                  title={post.title}
+                  image={post.img}
+                  key={post.title}
+                  desc={post.subheading}
+                />
+              );
+            })}
+        {selectedTags?.length === 0 &&
+          posts.map((post) => {
+            return (
+              <BlogCard
+                id={post._id}
+                tag={post.tag}
+                title={post.title}
+                image={post.img}
+                key={post.title}
+                desc={post.subheading}
+              />
+            );
+          })}
       </div>
     </main>
-  )
+  );
 }
